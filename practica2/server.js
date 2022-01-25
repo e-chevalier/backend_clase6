9953383319c1c0bd15c38312dd18f2df6640bd2a
e-chevalier <echevalier.dev@gmail.com>
2022-01-25 12:10:20 -0300

    Clase6 Practica 1 - Parte3

--- practica2/server.js
+++ practica2/server.js
@@ -12,14 +12,25 @@ app.use(express.urlencoded({ extended: true }))
 app.set('views', './views'); // especifica el directorio de vistas
 app.set('view engine', 'ejs'); // registra el motor de plantillas
 
-// Indicamos que queremos cargar los archivos estáticos que se encuentran en dicha carpeta
+
 app.use(express.static('./public'))
 app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'))
-// Esta ruta carga nuestro archivo index.html en la raíz de la misma
 
+const messages = []
+
+io.on('connection', (socket) => {
+    
+    console.log('¡Nuevo cliente conectado!')  // - Pedido 1
+    socket.on('newMessage', (data) => {
+        messages.push({ socketid: socket.id, mensaje: data})
+        console.log(messages)
+        io.sockets.emit('messages', messages)
+    })
+    
+})
 
 app.get('/', (req, res) => { 
-    res.render('index', {productos: "Hola"})
+    res.render('index', {messages: messages})
 })
 
 // El servidor funcionando en el puerto 3000
@@ -30,15 +41,3 @@ httpServer.listen(PORT, () => {
 
 httpServer.on("error", error => console.log(`Error en servidor ${error}`))
 
-const mensajes = []
-
-io.on('connection', (socket) => {
-    // "connection" se ejecuta la primera vez que se abre una nueva conexión
-    console.log('¡Nuevo cliente conectado!')  // - Pedido 1
-    socket.on('newMessage', (data) => {
-        mensajes.push(data)
-        console.log(mensajes)
-        io.sockets.emit('messages', data)
-    })
-    
-})
